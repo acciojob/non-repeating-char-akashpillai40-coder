2c8be073556032f48be8c5feb8dd6be63b3d63ce
akashpillai40-coder <akashpillai40@gmail.com>
2026-03-01 12:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    if (!str) return null;
+    
+    //counting frequency of char
+    let count = {};
+    for (let char of str) {
+        if (count[char]) {
+            count[char] = count[char] + 1;
+        }
+        else{
+            count[char] = 1;
+        }
+    }
+    
+    for(let char of str) {
+        if(count[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
